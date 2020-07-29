9c8317232f439639cd6b5474cd9c5b0c97fc3eb0
MaddieBurbage <madeline.g.burbage@gmail.com>
2020-07-29 02:00:53 +0100

    Changed CSR address to a macro

--- readCSR.c
+++ readCSR.c
@@ -6,45 +6,38 @@
 #include <unistd.h>
 #include "encoding.h"
 
+// *** PARAMS ***
+#ifndef ADDR
+#define ADDR 0xc00 // address of the CSR to read
+#endif
+
+// Macros for allowing ADDR to be stringified
+#define readCSR(addr) read_csr(addr)
+#define readAddress readCSR(ADDR)
+
 /** Read and print a single CSR's value
- * address: the CSR address to view (defined in rocket/Instructions.scala)
  * returns: the CSR's value
  */
-long readFromCSR(long address) {
-  long csrValue;
-  asm volatile ("csrr %[rd], %[csr]": [rd]"=r"(csrValue) : [csr]"r"(address));
-  printf("Value at %lu: %lu \n", address, csrValue);
+long readFromCSR() {
+  long csrValue = readAddress;
+  printf("Value at %d: %lu \n", ADDR, csrValue);
   return csrValue;
 }
 
 int main(int argc, char* argv[]) {
-  long csrValue = 0;
-  unsigned long address;
-  char *endptr;
   int repetitions;
   
   if(argc < 2) {
-    fprintf(stderr, "Usage: %s address [repetitions]\n", argv[0]);
-    fprintf(stderr, "address: a hex (0x__) octal (00__) or decimal CSR address\n");
+    fprintf(stderr, "Usage: %s repetitions\n", argv[0]);
     fprintf(stderr, "repetitions: how many seconds in a row to check the values\n");
     exit(-1);
   }
-
-  //Convert arg 2 to a long (base 16 8 or 10)
-  address = strtol(argv[1], &endptr, 0); 
-  if(endptr == argv[1]) {
-    fprintf(stderr, "Error: no digits found\n");
-    exit(-1);
-  }
-
-  if(argc == 3) {
-    repetitions = atoi(argv[2]);
-  }
+  repetitions = atoi(argv[1]);
 
   //Read from the CSR repetitions times
   for(int i = 0; i < repetitions; i++) {
     sleep(1);
-    readFromCSR(address);
+    readFromCSR();
   }
   
 }
